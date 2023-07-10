1d06ef7a733c4d3368043e49590538332f6cb8dc
Aldrich Lapid <alapid@LABA.PSWAS.LOCAL>
2023-07-10 14:05:36 -0700

    added project 1

---
+++ src/Details.js
@@ -0,0 +1,99 @@
+// Enter all your detials in this file
+// Logo images
+// import logogradient from "./assets/logo.svg";
+// import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/pfpforsweportfolio.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/rickandmorty thumbnail .png";
+
+
+// Logos
+// export const logos = {
+//   logogradient: logogradient,
+//   logo: logo,
+// };
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Pavan MG",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "www.linkedin.com/in/aldrichgranlapid",
+  github: "https://github.com/a1dric5",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Product Design Lead",
+    Company: `breathing.ai`,
+    Location: "New York City - Remote",
+    Type: "Full Time",
+    Duration: "April 2023 - Present",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Software Engineer",
+    Company: "Per Scholas",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "March 2023 - July 2023",
+  },
+  {
+    Position: "Bachelor in Linguistics",
+    Company: `University of Washington`,
+    Location: "Seattle, Washington",
+    Type: "Full Time",
+    Duration: "Sept 2019 - June 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  tailwind: tailwind,
+  vscode: vscode,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Rick & Morty Streaming Services Website",
+    image: projectImage1,
+    description: `Using React, HTML, CSS, and Javascript, I created a streaming service web app that generates randomized characters from a specific episode along with a quiz`,
+    techstack: "HTML/CSS, JavaScript, React",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/a1dric5/Mod-2-Project",
+  }
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "aldrichlapid@gmail.com",
+  phone: "+ 1 (206)-856-4896",
+};
